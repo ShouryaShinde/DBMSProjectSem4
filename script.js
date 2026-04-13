e4e5e13e051333a0691e3eb35a43df7afac4ba80
Shourya Shinde <shindeshauryaanil@gmail.com>
2026-04-13 23:39:26 +0530

    Feature : Login Page

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 import express from "express" ;
+import bodyParser from "body-parser";
 import axios from "axios" ;
 
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +14,13 @@ app.use(express.json());
 app.set("view engine", "ejs");
 
 app.get("/" , (req,res) => {
-  res.render("home") ;
+  res.render("auth/Login") ;
+});
+
+app.post("/login" , (req ,res) => {
+  const {email , password , action} = req.body ;
+  console.log(action) ;
+  console.log(email , password) ;
 });
 
 app.listen(port , () => {
