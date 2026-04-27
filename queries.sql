3ae53d212d753a0063b1374fcc464db5ab970afb
Vaishnav Dnyaneshwar Babar <vaishnavbabar@Vaishnavs-MacBook-Air.local>
2026-04-27 14:59:33 +0530

    Update Queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
 INSERT INTO (first_name , last_name , phone) VALUES ($1 , $2 , $3) WHERE email = $4 ;
 [firstName , lastName , phone , email]
 
+INSERT INTO (first_name , last_name , phone) VALUES ($1 , $2 , $3) WHERE email = $4 ;
+[firstName , lastName , phone , email]
+
